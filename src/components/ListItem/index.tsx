e447932520141ae6c1ba844b7f306c38ea7ea51b
Yanina Lysukha <yanina.lysukha@gmail.com>
2025-01-28 20:00:20 +0100

    feat: create loader component

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import './style.scss';
 
 export default class ListItem extends Component {
     render() {
---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './style.scss';
+
+export default class Loader extends Component {
+    render() {
+        return (
+            <img
+                className="loader"
+                src="../../assets/icons/ring-inscription.png"
+                alt="loader icon"
+            ></img>
+        );
+    }
+}
