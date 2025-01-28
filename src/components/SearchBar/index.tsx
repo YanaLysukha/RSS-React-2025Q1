184580ca3c01763458393ba22fff69ef0fce78e9
Yanina Lysukha <yanina.lysukha@gmail.com>
2025-01-28 17:30:52 +0100

    feat: add accessibility

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -9,7 +9,11 @@ export default class SearchBar extends Component {
                     <img className="logo" src="src/assets/icons/LOTR-icon.svg" alt="App Logo" />
                 </div>
                 <form className="search-bar">
+                    <label htmlFor="search" className="sr-only">
+                        Search Input
+                    </label>
                     <input
+                        id="search"
                         type="text"
                         placeholder="Find your favorite character..."
                         className="search-input"
