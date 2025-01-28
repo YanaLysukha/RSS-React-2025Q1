ed97675d4b6edfac801d7d053a7dbd0986f8b0b6
Yanina Lysukha <yanina.lysukha@gmail.com>
2025-01-28 19:55:33 +0100

    feat: create list item component

---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class ListItem extends Component {
+    render() {
+        return (
+            <div className="list-item">
+                <h1 className="title"></h1>
+            </div>
+        );
+    }
+}
