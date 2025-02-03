5c8e1e2ddec12f92eab6fe6aa59fdf016db54a71
Yanina Lysukha <yanina.lysukha@gmail.com>
2025-02-03 15:45:38 +0100

    refactor: migrate some class components to functional components

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import styles from "./style.module.scss";
 import { ICharacter } from "../../api/types";
 
@@ -6,23 +5,19 @@ type TListItemProps = {
     character: ICharacter;
 };
 
-export default class ListItem extends Component<TListItemProps> {
-    render() {
-        const { character } = this.props;
-
-        return (
-            <div className={styles.listItem}>
-                <h1 className={styles.title}>{character.name}</h1>
-                <p className={styles.highlighted}>
-                    Race: <span>{character.race}</span>
-                </p>
-                <p className={styles.highlighted}>
-                    Gender: <span>{character.gender}</span>
-                </p>
-                <p className={styles.highlighted}>
-                    Spouse: <span>{character.spouse}</span>
-                </p>
-            </div>
-        );
-    }
+export default function ListItem({ character }: TListItemProps) {
+    return (
+        <div className={styles.listItem}>
+            <h1 className={styles.title}>{character.name}</h1>
+            <p className={styles.highlighted}>
+                Race: <span>{character.race}</span>
+            </p>
+            <p className={styles.highlighted}>
+                Gender: <span>{character.gender}</span>
+            </p>
+            <p className={styles.highlighted}>
+                Spouse: <span>{character.spouse}</span>
+            </p>
+        </div>
+    );
 }
--- src/components/ListView/index.tsx
+++ src/components/ListView/index.tsx
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import { ICharacter } from "../../api/types";
 import ListItem from "../ListItem";
 import styles from "./style.module.scss";
@@ -7,23 +6,20 @@ type TListViewProps = {
     characters: ICharacter[];
 };
 
-export default class ListView extends Component<TListViewProps> {
-    render() {
-        const { characters } = this.props;
-        if (!characters.length) {
-            return (
-                <div className={styles.listViewMessage}>
-                    Our story has hit a small snag, and the characters you seek are momentarily out
-                    of reach...
-                </div>
-            );
-        }
+export default function ListView({ characters }: TListViewProps) {
+    if (!characters.length) {
         return (
-            <div className={styles.listViewWrapper}>
-                {characters.map((character) => (
-                    <ListItem character={character} key={character._id}></ListItem>
-                ))}
+            <div className={styles.listViewMessage}>
+                Our story has hit a small snag, and the characters you seek are momentarily out of
+                reach...
             </div>
         );
     }
+    return (
+        <div className={styles.listViewWrapper}>
+            {characters.map((character: ICharacter) => (
+                <ListItem character={character} key={character._id}></ListItem>
+            ))}
+        </div>
+    );
 }
